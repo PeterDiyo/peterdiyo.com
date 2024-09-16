999a6cb683ef76886cf03d65c32096a13f2d0d3a
unknown <PeterDiyo>
2024-09-16 22:16:37 +0530

    another commit

--- src/data.jsx
+++ src/data.jsx
@@ -4,6 +4,7 @@ import imgs from "./assets/projects/imgs.png";
 import menu from "./assets/projects/menu.png";
 import Viz from "./assets/projects/Visualizer.png";
 import Shopcart from "./assets/projects/shopcart.png";
+import DiyoStore from "./assets/projects/dstore.png";
 
 import Literature from "./assets/GalleryImages/lit.jpg";
 import Java from "./assets/GalleryImages/jav.jpg";
@@ -16,10 +17,14 @@ import Nfair from "./assets/GalleryImages/Nfair.jpg";
 import Rfair from "./assets/GalleryImages/Rfair.jpg";
 import Headboy from "./assets/GalleryImages/Headboy.jpg";
 import SII from "./assets/GalleryImages/SII.jpg";
+import SWE from "./assets/GalleryImages/SWE.jpg";
+import Node from "./assets/GalleryImages/Node.jpg";
 
 import { nanoid } from "nanoid";
 import { DiMongodb, DiMysql, DiVisualstudio, DiHtml5 } from "react-icons/di";
 import { FaJs, FaNodeJs, FaPython, FaReact, FaGithub } from "react-icons/fa";
+import { SiNextdotjs, SiTypescript } from "react-icons/si";
+import { TbBrandReactNative } from "react-icons/tb";
 
 export const links = [
   { id: nanoid(), href: "#home", text: "home" },
@@ -33,13 +38,19 @@ export const links = [
 export const skills = [
   {
     id: nanoid(),
+    title: "Next.js",
+    icon: <SiNextdotjs className="h-16 w-16 text-gray-300" />,
+    text: "Next.js for building scalable and robust full stack appliaction with server components and server-side render",
+  },
+  {
+    id: nanoid(),
     title: "HTML & CSS",
     icon: <DiHtml5 className="h-16 w-16 text-gray-300" />,
     text: "Highly skilled in HTML & CSS, creating visually appealing and responsive websites for optimal user experience.",
   },
   {
     id: nanoid(),
-    title: "React",
+    title: "React.js",
     icon: <FaReact className="h-16 w-16 text-gray-300" />,
     text: "Advanced proficiency in React, a strong emphasis on component-based architecture.",
   },
@@ -69,6 +80,12 @@ export const skills = [
   },
   {
     id: nanoid(),
+    title: "TypeScript",
+    icon: <SiTypescript className="h-16 w-16 text-gray-300" />,
+    text: "Advanced knowledge in TypeScript, a superset of JavaScrip that adds static typing with optional type annotations to JavaScript.",
+  },
+  {
+    id: nanoid(),
     title: "Python",
     icon: <FaPython className="h-16 w-16 text-gray-300" />,
     text: "A strong understanding in python for problem solving and competitve programming.",
@@ -85,9 +102,23 @@ export const skills = [
     icon: <DiVisualstudio className="h-16 w-16 text-gray-300" />,
     text: "I utilize VS Code extensively, a source code editor, in my development projects due to its intuitive interface, robust features, and extensive extension marketplace.",
   },
+  {
+    id: nanoid(),
+    title: "React Native",
+    icon: <TbBrandReactNative className="h-16 w-16 text-gray-300" />,
+    text: "React Native for building Android and iOS Native mobile applications.",
+  },
 ];
 
 export const projects = [
+  {
+    id: nanoid(),
+    img: DiyoStore,
+    url: "https://diyostore-next-js.vercel.app/",
+    github: "https://github.com/PeterDiyo/Diyostore-Next.js",
+    title: "Diyostore",
+    text: "A Next.js Application. An online shopping platform that allows users to browse products, provide reviews, add to Cart, share, make orders and more. It has an admin Dashboard to manage all products.",
+  },
   {
     id: nanoid(),
     img: DiyoMix,
@@ -95,6 +126,22 @@ export const projects = [
     github: "https://github.com/PeterDiyo/DiyoMix",
     title: "DiyoMix",
     text: "With a flick of your finger, you'll unlock a treasure trove of enchanting drink recipes that'll make your taste buds dance and your friends jump with joy. Get ready to shake up your mixology game with DiyoMix.",
+  },
+  {
+    id: nanoid(),
+    img: Viz,
+    url: "https://peterdiyo.github.io/Pathfinding_Visualizer/",
+    github: "https://github.com/PeterDiyo/Pathfinding_Visualizer",
+    title: "Pathfinding Vizualizer (Dijkstra)",
+    text: "Starting point (source) is green box and ending point (target) is a red box, draw walls to make routes and click on the visualize dijkstra algorithm button to get the shortest path.",
+  },
+  {
+    id: nanoid(),
+    img: Shopcart,
+    url: "https://shopping-cart-diyo.netlify.app/",
+    github: "https://github.com/PeterDiyo/Shopping-Cart",
+    title: "Shopping Cart - Redux Toolkit",
+    text: "Effective management of state, simplifying various aspects of Redux development by providing concise APIs for common tasks like creating Redux stores, writing reducers, and creating actions",
   },
   {
     id: nanoid(),
@@ -112,33 +159,15 @@ export const projects = [
     title: "search images",
     text: "Quickly get any images by simply searching for a specific image, whether for your personal projects or your designs, discover thousands of images that might be useful to you. choose dark or light mode.",
   },
-  {
-    id: nanoid(),
-    img: Viz,
-    url: "https://peterdiyo.github.io/Pathfinding_Visualizer/",
-    github: "https://github.com/PeterDiyo/Pathfinding_Visualizer",
-    title: "Pathfinding Vizualizer (Dijkstra)",
-    text: "Starting point (source) is green box and ending point (target) is a red box, draw walls to make routes and click on the visualize dijkstra algorithm button to get the shortest path.",
-  },
-  {
-    id: nanoid(),
-    img: menu,
-    url: "https://menu-diyo.netlify.app/",
-    github: "https://github.com/PeterDiyo/menu",
-    title: "ease menu",
-    text: "easily filter your food menu preferences with only a single click whether you are looking for your favorite lunch menu, or you want to try out some delicious zambian dishes.",
-  },
-  {
-    id: nanoid(),
-    img: Shopcart,
-    url: "https://shopping-cart-diyo.netlify.app/",
-    github: "https://github.com/PeterDiyo/Shopping-Cart",
-    title: "Shopping Cart - Redux Toolkit",
-    text: "Effective management of state, simplifying various aspects of Redux development by providing concise APIs for common tasks like creating Redux stores, writing reducers, and creating actions",
-  },
 ];
 
 export const galleryData = [
+  {
+    id: nanoid(),
+    imageUrl: SWE,
+    description:
+      "Software Engineering Certification from IBM, with Honors - 2024",
+  },
   {
     id: nanoid(),
     imageUrl: Literature,
@@ -160,6 +189,12 @@ export const galleryData = [
     imageUrl: MathO,
     description:
       "Best performing student with O grade in Statistical Methods - 2024",
+  },
+  {
+    id: nanoid(),
+    imageUrl: Node,
+    description:
+      "Node.js and MongoDB: Developing Back-end Applications, IBM - 2024",
   },
   {
     id: nanoid(),
@@ -188,12 +223,6 @@ export const galleryData = [
     imageUrl: Nfair,
     description:
       "Won first prize in Maths Innovations, National J.E.T.S fair, Zambia - 2022",
-  },
-  {
-    id: nanoid(),
-    imageUrl: Rfair,
-    description:
-      "Won first prize in Maths Innovations, Lusaka regional J.E.T.S fair - 2022",
   },
   {
     id: nanoid(),
